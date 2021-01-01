ae08b8089f3a6c69d869621b1074fe4a12aeaf01
Xeno Meienberg <xenomeienberg@gmx.ch>
2021-01-01 12:22:03 +0100

    commit on 'master'

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,17 @@
+let js= 'amazing';
+console.log(40+8+23-10);
+
+console.log("Xeno");
+console.log(23)
+
+let firstName = "Matilda";
+
+console.log(firstName);
+
+
+let jonas_matilda = "JM";
+let sfunction = 27;
+let PI = 3.1415;
+
+
+
