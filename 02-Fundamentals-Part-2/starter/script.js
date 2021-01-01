126b70b3d9e35a5e3901e51ae4855fbc1ca2cf0b
Xeno Meienberg <xenomeienberg@gmx.ch>
2021-01-02 00:22:09 +0100

    commit on master

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -45,6 +45,8 @@ function describePopulation(country, population){
 describePopulationSwitzerland = describePopulation('Switzerland', 8.46);
 describePopulationUSA = describePopulation('USA',331);
 describePopulationUK = describePopulation('UK', 66.65)
+
+
 // Coding Challenge 1
 
 const calcAverage = (scoreOne,scoreTwo,scoreThree)=> (scoreOne+scoreTwo+scoreThree)/3;
@@ -70,6 +72,22 @@ const checkWinner = (avgDolphins,avgKoalas) => {
 }
 
 checkWinner(avgDolphins,avgKoalas)
+
+
+// Introduction to Arrays
+
+populations = [8.46,331,66.5,1393];
+
+console.log(populations.length == 4);
+
+let percentages = [];
+
+percentages[0] = percentageOfWorld1(populations[0]);
+percentages[1] = percentageOfWorld1(populations[1]);
+percentages[2] = percentageOfWorld1(populations[2]);
+percentages.push(percentageOfWorld1(populations[populations.length-1]))
+
+console.log(percentages)
 
 // Basic Array Operations
 
@@ -132,17 +150,101 @@ console.log(myCountry)
 
 // Dot vs. Bracket Notation
 
+console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language} speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}. `);
 
+myCountry.population += 2;
+console.log(myCountry.population)
+myCountry['population'] -= 2;
+console.log(myCountry.population)
 
 // Coding Challenge 3
 
+const Mark = {
+    firstName : 'Mark',
+    lastName : 'Miller',
+    mass : 78,
+    height: 1.69
+
+}
+
+const John = {
+    firstName : 'John',
+    lastName : 'Smith',
+    mass : 92,
+    height : 1.95
+}
+
+function calcBMI(mass, height){
+    return mass/height**2;
+}
+
+if (calcBMI(Mark.mass,Mark.height) > calcBMI(John.mass,John.height)){
+    console.log(`Mark's BMI (${calcBMI(Mark.mass,Mark.height)}) is higher than John's (${calcBMI(John.mass,John.height)}) `)
+} else {
+    console.log(`$John's BMI (${calcBMI(John.mass,John.height)}) is higher than Mark's (${calcBMI(Mark.mass,Mark.height)}) `)
+}
+
 // Iteration: The for loop
+
+for (let voter = 1; voter <= 50; voter++) {
+    console.log(`Voter number ${voter} is currently voting`);
+}
 
 // Looping Arrays, Breaking and Continuing
 
+let percentages2 = [];
+
+for (let index = 0; index < populations.length; index++) {
+    percentages2.push(percentageOfWorld1(populations[index]));
+}
+
+console.log(percentages2)
+
+for (let index = 0; index < populations.length; index++) {
+    console.log(percentages[index] == percentages2[index])
+}
+
 // Looping Backwards and Loops in Loops
+
+
+let listOfNeighbours = [['Canada','Mexico'],['Spain'],['Norway','Sweden','Russia']];
+console.log(listOfNeighbours.length)
+console.log(listOfNeighbours[0])
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    const max = listOfNeighbours[i]
+    console.log(`Country: ${max[0]}`) 
+    if(max.length>1){
+        for (let  j= 1; j < max.length; j++) {
+            if(max.length>1){
+            console.log(`Neighbour: ${max[j]}`)
+            }
+        }
+    }
+    
+}
 
 // The while loop
 
+
+voter = 1
+while (voter <= 50) 
+{
+    console.log(`Voter number ${voter} is currently voting`)
+    voter++;
+}
+
 // Coding Challenge 4
+
+newBills = [10,292,176,440,37,105,10,1100,86,52];
+let tips = [];
+let totals = [];
+
+for (let index = 0; index < newBills.length; index++) {
+    tips.push(calcTip(newBills[index]))
+    totals.push(tips[index]+newBills[index])
+}
+
+console.log(tips)
+console.log(totals)
 
