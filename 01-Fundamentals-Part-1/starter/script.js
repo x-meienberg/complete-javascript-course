7540d8abe9ddf99ba7827e60aed92d73629d9bdf
Xeno Meienberg <xenomeienberg@gmx.ch>
2021-01-01 18:47:56 +0100

    commit on 'master'

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -116,6 +116,70 @@ console.log(5+6+'4'+9-4+2)
 
 // Equality Operators
 
+let numNeighbors = prompt('How many neighbours does your country have?');
+numNeighbors = Number(numNeighbors);
+
+if (numNeighbors == 1) {
+    console.log('Only 1 border')
+} else if (numNeighbors > 1) {
+    console.log('More than 1 border')
+} else {
+    console.log('No borders')
+}
 
 
 // Logical Operators
+
+if (language == 'English' && population < 50000000 && false==isIsland){
+    console.log(`You should live in ${country} :)`)
+} else  {
+    console.log(`${country} does not meet your criteria :(`)
+}
+
+// Coding Challenge 3
+
+function averageScore(firstScore,secondScore,thirdScore){
+    return (firstScore+secondScore+thirdScore)/3
+}
+
+// Data 1
+let averageScoreDolphins = averageScore(96,108,89);
+let averageScoreKoalas = averageScore(88,91,110);
+
+// Data Bonus 1
+averageScoreDolphins = averageScore(97,112,101);
+averageScoreKoalas = averageScore(109,95,123);
+
+// Data Bonus 2
+averageScoreDolphins = averageScore(97,112,101);
+averageScoreKoalas = averageScore(109,95,106)
+
+
+if (averageScoreDolphins > averageScoreKoalas && averageScoreDolphins >100){
+    console.log('Dolphins win')
+} else if (averageScoreDolphins < averageScoreKoalas && averageScoreKoalas >100) {
+    console.log('Koalas win')
+} else if (averageScoreDolphins == averageScoreKoalas && averageScoreDolphins > 100){
+    console.log('Draw')
+} else {
+    console.log('Nobody wins')
+}
+
+
+// The switch Statement
+
+language = 'english';
+switch (language) {
+    case 'chinese' || 'mandarin': console.log('MOST number of native speakers'); break;
+    case 'spanish': console.log('2nd place in number of native speakers'); break;
+    case 'english': console.log('3rd place'); break;
+    case 'hindi': console.log('Number 4'); break;
+    case 'arabic': console.log('5th most spoken language'); break;
+    default: console.log('Great language too :D'); break;
+
+}
+
+// The conditional (ternery) Operator
+
+
+
