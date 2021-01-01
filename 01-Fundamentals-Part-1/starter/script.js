5cef16cca04df9b8133b7b2182b064e2e1897e37
Xeno Meienberg <xenomeienberg@gmx.ch>
2021-01-01 18:06:28 +0100

    commit

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -108,6 +108,14 @@ if (BMIMark>BMIJohn){
 
 // Type Conversion and Coersion
 
+console.log('9'-'5');
+console.log('19'-'13'+'17');
+console.log('19'-'13'+17);
+console.log('123' < 57);
+console.log(5+6+'4'+9-4+2)
+
 // Equality Operators
+
+
 
 // Logical Operators
